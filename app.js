79a63cdfb852b3f1fdef11a7542098683c41b826
ZouLifang <2396172850@qq.com>
2021-04-25 13:36:26 +0800

    添加session

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const path = require('path')
 const router = require('./router/router.js')
+const session =  require('express-session')
+
 const app = express()
 
-app.use('/node_modules/',express.static('./node_modules'))
-app.use('/public/',express.static('./public'))
+app.use('/node_modules/',express.static( path.join(__dirname + '/node_modules') ))
+app.use('/public/',express.static( path.join(__dirname + '/public') ))
 app.engine('html',require('express-art-template'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(router)
 
--- router/router.js
+++ router/router.js
@@ -1,5 +1,7 @@
 const {Router} = require('express')
 const User = require('../model/user')
+//用md5给密码加密
+const md5 = require('blueimp-md5')
 // 数据模型大写
 const router = new Router()
 
@@ -7,6 +9,37 @@ router.get('/login',(req,res)=>{
   res.render('login.html')
 }
 )
+
+router.post('/login',(req,res)=>{
+ let body = req.body
+
+  User.findOne({
+    email:body.email,
+    password:md5(md5(req.body.password))
+  })
+
+  .then((data)=>{
+    if(data){
+      req.session.user = data
+      
+      return res.status(200).json({
+        err_code:0,
+        message:'valid'
+      })
+    }
+      //  密码加密（两次）
+    return res.status(200).json({
+      err_code:1,
+      message:'email or password invalid'
+    })
+  })
+
+  .catch((err)=>{res.status(500).json({
+    err_code:500,
+    message:'Server Error'
+  })})
+
+})
 
 router.get('/register',(req,res)=>{
   res.render('register.html')
@@ -18,24 +51,40 @@ router.post('/register',(req,res)=>{
     //判断用户是否存在
     //若存在则不能创建用户
 
-    // 判断是否同时满足：{$OR:[{email:req.body.email},{nickName:req.body.nickName}]}
-
+    // 判断是否同时满足：{$OR:[{email:req.body.email},{nickName:req.body.nickName}]}  
     User.findOne({email:req.body.email})
-    .then((data)=>{
-      if(data){
-        return res.status(200).json({err_code:1,message:'email has been exist'})
-      }
+      .then((data)=>{
+        if(data){
+          return res.status(200).json({err_code:1,message:'email has been exist'})
+        }
+          //  密码加密（两次）
+        req.body.password = md5(md5(req.body.password))
 
-      let user = new User(req.body)
-      user.save(user)
-      .then(()=>{res.status(200).json({err_code:0,message:'register successully'})})
+        let user = new User(req.body)
+        user.save(user)
+        .then(()=>{
+          //把用户信息用session存储起来
+          req.session.user = user
+
+          res.status(200).json({err_code:0,message:'register successully'})
+        })
+        .catch((err)=>{res.status(500).json({err_code:500,message:'Server Error'})})    
+
+      })
       .catch((err)=>{res.status(500).json({err_code:500,message:'Server Error'})})
-      
     
-    })
-    .catch((err)=>{res.status(500).json({err_code:500,message:'Server Error'})})
+})
 
-    // User.find().then((data)=>{console.log(data)})
+router.get('/',(req,res)=>{
+  res.render('home.html',{
+      user: req.session.user
+  })
+
+})
+
+router.get('/logout',(req,res)=>{
+  req.session.user = null
+  res.redirect('/')
 })
 
 
